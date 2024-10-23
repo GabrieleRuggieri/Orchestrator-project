03c3eb34f363b7f6d907d311ad2bd0fd349e4226
GabrieleRuggieri <gabri20022002@gmail.com>
2024-10-23 23:02:16 +0200

    Fix GlobalControllerExceptionHandler InventoryService

--- inventory_service/src/main/java/com/patroclos/controller/GlobalControllerExceptionHandler.java
+++ inventory_service/src/main/java/com/patroclos/controller/GlobalControllerExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.patroclos.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,16 +9,15 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalControllerExceptionHandler {
-	
-	private static org.slf4j.Logger Logger = LoggerFactory.getLogger(GlobalControllerExceptionHandler.class);
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<String> handleConnversion(RuntimeException ex) {
-    	Logger.info(ex.getMessage());
-    	ex.printStackTrace();
+        log.info(ex.getMessage());
+        ex.printStackTrace();
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 }
