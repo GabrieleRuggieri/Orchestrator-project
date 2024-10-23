e6198ebcee6ab5538b4d045c6b45897cb9c1992e
GabrieleRuggieri <gabri20022002@gmail.com>
2024-10-23 23:02:31 +0200

    Fix WebSecurityConfig InventoryService

--- inventory_service/src/main/java/com/patroclos/configuration/WebSecurityConfig.java
+++ inventory_service/src/main/java/com/patroclos/configuration/WebSecurityConfig.java
@@ -1,6 +1,6 @@
 package com.patroclos.configuration;
 
-import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -8,8 +8,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -20,63 +20,72 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-
 @Configuration
 public class WebSecurityConfig {
-	
-	@Value("${server.port}")
-	private String port;
-	public final static String[] PUBLIC_REQUEST_MATCHERS = { "/api/v1/auth/**", "/api-docs/**", "/swagger-ui/**" };
-	
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {	
-		http
-		.cors()
-		.disable()
-		.csrf()
-		.disable();
-		
-		return http.build();
-	}
 
-	@Bean
-	public UserDetailsService users() {
-		// The builder will ensure the passwords are encoded before saving in memory
-		UserBuilder users = User.builder();
-		UserDetails user = users
-				.username("user")
-				.password(passwordEncoder().encode("password"))
-				.roles("USER")
-				.build();
-		UserDetails admin = users
-				.username("admin")
-				.password(passwordEncoder().encode("password"))
-				.roles("USER", "ADMIN")
-				.build();
-		return new InMemoryUserDetailsManager(user, admin);
-	}
+    @Value("${server.port}")
+    private String port;
 
-	@Bean
-	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-		return config.getAuthenticationManager();
-	}
+    private static final String[] PUBLIC_REQUEST_MATCHERS = {"/api/v1/auth/**", "/api-docs/**", "/swagger-ui/**"};
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                //.csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(PUBLIC_REQUEST_MATCHERS).permitAll()
+                        .anyRequest().authenticated());
 
-	@Bean
-	public CorsConfigurationSource corsConfigurationSource() {
-		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration corsConfiguration = new CorsConfiguration().applyPermitDefaultValues();
-		corsConfiguration.setAllowedOrigins(
-				Arrays.asList(
-						String.format("http://localhost:%s", port), 
-						String.format("https://localhost:%s", port)));
-		corsConfiguration.setAllowedMethods(Arrays.asList("GET","POST"));
-		source.registerCorsConfiguration("/**", corsConfiguration);
-		return source;
-	}
+        return http.build();
+    }
 
+    @Bean
+    public UserDetailsService users() {
+        UserDetails user = createUser("user", "USER");
+        UserDetails admin = createUser("admin", "USER", "ADMIN");
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    /*private UserDetails createUser(String username, String password, String... roles) {
+        return User.builder()
+                .username(username)
+                .password(passwordEncoder().encode(password))
+                .roles(roles)
+                .build();
+    }*/
+
+    private UserDetails createUser(String username, String... roles) {
+        return User.builder()
+                .username(username)
+                .password(passwordEncoder().encode("password"))
+                .roles(roles)
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(List.of(
+                String.format("http://localhost:%s", port),
+                String.format("https://localhost:%s", port)));
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST"));
+        corsConfiguration.setAllowCredentials(true); // Consente l'invio di credenziali
+        corsConfiguration.setAllowedHeaders(List.of("*")); // Permette tutti gli headers
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
+    }
 }
