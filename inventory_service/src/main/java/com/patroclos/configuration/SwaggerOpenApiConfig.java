1a02b0534cc73b1182f17bd4334bca72b2cf3aa0
GabrieleRuggieri <gabri20022002@gmail.com>
2024-10-23 23:02:45 +0200

    Fix Swagger InventoryService

--- inventory_service/src/main/java/com/patroclos/configuration/SwaggerOpenApiConfig.java
+++ inventory_service/src/main/java/com/patroclos/configuration/SwaggerOpenApiConfig.java
@@ -7,27 +7,41 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 
 @Configuration
 @SecurityScheme(
-		  name = "Bearer Authentication",
-		  type = SecuritySchemeType.HTTP,
-		  bearerFormat = "JWT",
-		  scheme = "bearer"
-		)
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
 public class SwaggerOpenApiConfig {
 
-	@Bean
-	public OpenAPI springOpenAPI() {
-		return new OpenAPI()
-				.info(new Info().title("SpringBoot API")
-						.description("SpringBoot sample application")
-						.version("v0.0.1")
-						.license(new License().name("Apache 2.0").url("http://springdoc.org")))
-				.externalDocs(new ExternalDocumentation()
-						.description("SpringBoot Wiki Documentation")
-						.url("https://springboot.wiki.github.org/docs"));
-	}
+    private static final String API_TITLE = "SpringBoot API";
+    private static final String API_DESCRIPTION = "SpringBoot sample application";
+    private static final String API_VERSION = "v0.0.1";
+    private static final String LICENSE_NAME = "Apache 2.0";
+    private static final String LICENSE_URL = "https://opensource.org/licenses/Apache-2.0";
+    private static final String WIKI_URL = "https://springboot.wiki.github.org/docs";
+    private static final String CONTACT_NAME = "Support Team";
+    private static final String CONTACT_EMAIL = "support@example.com";
+
+    @Bean
+    public OpenAPI springOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title(API_TITLE)
+                        .description(API_DESCRIPTION)
+                        .version(API_VERSION)
+                        .license(new License().name(LICENSE_NAME).url(LICENSE_URL))
+                        .contact(new Contact()
+                                .name(CONTACT_NAME)
+                                .email(CONTACT_EMAIL)))
+                .externalDocs(new ExternalDocumentation()
+                        .description("SpringBoot Wiki Documentation")
+                        .url(WIKI_URL));
+    }
 }
