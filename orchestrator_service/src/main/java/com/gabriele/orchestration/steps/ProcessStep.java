56717670fca14f8f9ddc65cd10fbe778cc2df2b5
GabrieleRuggieri <gabri20022002@gmail.com>
2024-11-18 22:01:33 +0100

    Added lombok

--- orchestrator_service/src/main/java/com/gabriele/orchestration/steps/ProcessStep.java
+++ orchestrator_service/src/main/java/com/gabriele/orchestration/steps/ProcessStep.java
@@ -1,18 +1,24 @@
 package com.gabriele.orchestration.steps;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import com.gabriele.enums.ProcessStepType;
 
 import reactor.core.publisher.Mono;
 
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public abstract class ProcessStep  {
 
 	protected WebClient webClient;
 	protected ProcessStepStatus status;
 	protected ProcessStepType type;
 	protected String error;
-	//
 	//	public abstract ProcessStepStatus getStatus();
 	//	public abstract ProcessStepType getType();
 	//	public abstract void setType(ProcessStepType type);
@@ -20,24 +26,4 @@ public abstract class ProcessStep  {
 	public abstract Mono<Boolean> process();
 	public abstract Mono<Boolean> revert();
 	public abstract ProcessStep copyStep();
-
-	public ProcessStepStatus getStatus() {
-		return this.status;
-	}
-
-	public void setStatus(ProcessStepStatus status) {
-		this.status = status;
-	}
-
-	public ProcessStepType getType() {
-		return this.type;
-	}
-
-	public void setType(ProcessStepType type) {
-		this.type = type;
-	}
-
-	public String getError() {
-		return this.error;
-	}
 }
