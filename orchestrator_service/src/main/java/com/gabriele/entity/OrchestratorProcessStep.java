d11429ab22c5c661eca1926a51996af52b9d0bdf
GabrieleRuggieri <gabri20022002@gmail.com>
2024-11-18 22:00:00 +0100

    Fix Entity

--- orchestrator_service/src/main/java/com/gabriele/entity/OrchestratorProcessStep.java
+++ orchestrator_service/src/main/java/com/gabriele/entity/OrchestratorProcessStep.java
@@ -13,20 +13,33 @@ import lombok.ToString;
 @ToString
 
 @Entity
-@Table(name = "orchestratorProcessStep")
+@Table(name = "ORCHESTRATOR_PROCESS_STEP")
 public class OrchestratorProcessStep {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "uuid")
     private String uuid;
 
     @Column(name = "orchestratorProcess_id")
-    private String orchestratorProcessId;
+    private Long orchestratorProcessId;
+
     @Column(name = "stepType")
     private String stepType;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "error")
     private String error;
+
     @Column(name = "statusStep")
-    private String statusStep;
+    private Long statusStep;
+
+    @ManyToOne
+    @JoinColumn(name = "id_orchestrator_process", nullable = false)
+    private OrchestratorProcess orchestratorProcess;
 }
