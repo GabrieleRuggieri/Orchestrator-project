d505fdf78a006cb934a3130cbd4edd3f1c5e2d80
GabrieleRuggieri <gabri20022002@gmail.com>
2024-11-19 21:50:08 +0100

    Lombok log

--- order_service/src/main/java/com/gabriele/controller/GlobalControllerExceptionHandler.java
+++ order_service/src/main/java/com/gabriele/controller/GlobalControllerExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.gabriele.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,15 +8,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalControllerExceptionHandler {
-
-	private static org.slf4j.Logger Logger = LoggerFactory.getLogger(GlobalControllerExceptionHandler.class);
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<String> handleConnversion(RuntimeException ex) {
-    	Logger.info(ex.getMessage());
+    	log.info(ex.getMessage());
     	ex.printStackTrace();
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
