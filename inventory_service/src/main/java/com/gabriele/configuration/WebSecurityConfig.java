76953572fce971dd92fb1555ac9379cca457a2fd
GabrieleRuggieri <gabri20022002@gmail.com>
2024-11-19 16:55:28 +0100

    Fix Web Security

--- inventory_service/src/main/java/com/gabriele/configuration/WebSecurityConfig.java
+++ inventory_service/src/main/java/com/gabriele/configuration/WebSecurityConfig.java
@@ -9,19 +9,15 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class WebSecurityConfig {
 
-    private static final String[] PUBLIC_REQUEST_MATCHERS = {
-            "/swagger-ui/**", // Swagger UI
-            "/v3/api-docs/**", // OpenAPI JSON
-            "/swagger-ui.html" // Swagger UI fallback
-    };
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable) // Disabilita CSRF
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(PUBLIC_REQUEST_MATCHERS).permitAll()
-                        .anyRequest().authenticated());
+                        .anyRequest().permitAll()) // Consenti l'accesso a tutte le richieste
+                .logout(AbstractHttpConfigurer::disable) // Disabilita il logout
+                .formLogin(AbstractHttpConfigurer::disable) // Disabilita il form login
+                .httpBasic(AbstractHttpConfigurer::disable); // Disabilita HTTP Basic Authentication
 
         return http.build();
     }
