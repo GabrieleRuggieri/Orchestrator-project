a0d39c6dfdd27fdd3f7fb93e51209ca23b4d0878
GabrieleRuggieri <gabri20022002@gmail.com>
2024-11-19 15:55:44 +0100

    Web Security configuration

--- inventory_service/src/main/java/com/gabriele/configuration/WebSecurityConfig.java
+++ inventory_service/src/main/java/com/gabriele/configuration/WebSecurityConfig.java
@@ -1,28 +1,31 @@
 package com.gabriele.configuration;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class WebSecurityConfig {
 
-    @Value("${server.port}")
-    private String port;
+    private static final String[] PUBLIC_REQUEST_MATCHERS = {
+            "/swagger-ui/**", // Swagger UI
+            "/v3/api-docs/**", // OpenAPI JSON
+            "/swagger-ui.html" // Swagger UI fallback
+    };
 
-    private static final String[] PUBLIC_REQUEST_MATCHERS = {"/api/v1/auth/**", "/api-docs/**", "/swagger-ui/**"};
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(PUBLIC_REQUEST_MATCHERS).permitAll()
+                        .anyRequest().authenticated());
+
+        return http.build();
+    }
+}
 
 //    @Bean
 //    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -36,42 +39,3 @@ public class WebSecurityConfig {
 //
 //        return http.build();
 //    }
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-                //.csrf(csrf -> csrf.disable())
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(PUBLIC_REQUEST_MATCHERS).permitAll()
-                        .anyRequest().authenticated());
-
-        return http.build();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-        return config.getAuthenticationManager();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(List.of(
-                String.format("http://localhost:%s", port),
-                String.format("https://localhost:%s", port)));
-        corsConfiguration.setAllowedMethods(List.of("GET", "POST"));
-        corsConfiguration.setAllowCredentials(true); // Consente l'invio di credenziali
-        corsConfiguration.setAllowedHeaders(List.of("*")); // Permette tutti gli headers
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfiguration);
-        return source;
-    }
-}
