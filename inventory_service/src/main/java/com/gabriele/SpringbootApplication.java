b1ab9b981eba020a36fede764fada6cf4af80b35
GabrieleRuggieri <gabri20022002@gmail.com>
2024-11-19 15:56:36 +0100

    Log lombok

--- inventory_service/src/main/java/com/gabriele/SpringbootApplication.java
+++ inventory_service/src/main/java/com/gabriele/SpringbootApplication.java
@@ -1,30 +1,24 @@
 package com.gabriele;
 
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.event.EventListener;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
-import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
-import io.r2dbc.spi.ConnectionFactory;
 
+@Slf4j
 @SpringBootApplication
 @ComponentScan(basePackages = "com.patroclos.*")
 public class SpringbootApplication {
-	
-	private static org.slf4j.Logger Logger = LoggerFactory.getLogger(SpringbootApplication.class);
 
-	public static void main(String[] args) {
-		//System.setProperty("server.servlet.context-path", "/inventory-service");
-		SpringApplication.run(SpringbootApplication.class, args);
-	} 
-	
-	@EventListener(ApplicationReadyEvent.class)
-	public void doSomethingAfterStartup() {
-	   Logger.info("INVENTORY SERVICE STARTED");
-	}
+    public static void main(String[] args) {
+        //System.setProperty("server.servlet.context-path", "/inventory-service");
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void doSomethingAfterStartup() {
+        log.info("INVENTORY SERVICE STARTED");
+    }
 }
