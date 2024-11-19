f9fb3a3f7ddbc819add04a1f5b9ccdf48927877e
GabrieleRuggieri <gabri20022002@gmail.com>
2024-11-19 23:42:42 +0100

    Added Swagger Order

--- order_service/src/main/java/com/gabriele/configuration/SwaggerOpenApiConfig.java
+++ order_service/src/main/java/com/gabriele/configuration/SwaggerOpenApiConfig.java
@@ -31,5 +31,5 @@ public class SwaggerOpenApiConfig {
 	}
 }
 
-//http://localhost:8080/swagger-ui.html
-//http://localhost:8080/v3/api-docs
+//http://localhost:8080/order-service/swagger-ui.html
+//http://localhost:8080/order-service/v3/api-docs
