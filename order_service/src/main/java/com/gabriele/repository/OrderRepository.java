763687b50e6518cec082911f449079bd518da722
GabrieleRuggieri <gabri20022002@gmail.com>
2024-11-19 23:42:29 +0100

    Fix Repo

--- order_service/src/main/java/com/gabriele/repository/OrderRepository.java
+++ order_service/src/main/java/com/gabriele/repository/OrderRepository.java
@@ -1,16 +1,25 @@
 package com.gabriele.repository;
 
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.gabriele.entitty.Order;
+import com.gabriele.entity.Order;
 
 import reactor.core.publisher.Mono;
 
-import java.util.UUID;
-
 @Repository
-public interface OrderRepository extends ReactiveCrudRepository<Order, UUID> {
+public interface OrderRepository extends ReactiveCrudRepository<Order, Long> {
 
-	Mono<Order> findById(UUID orderId);
+	@Query("SELECT o FROM Order o WHERE o.uuidOrder = :uuidOrder")
+	Mono<Order> findByUuidOrder(String uuidOrder);
+
+	@Query("SELECT o FROM Order o WHERE o.uuidCustomer = :uuidCustomer AND o.uuidItem = :uuidItem")
+	Mono<Order> findByUuidCustomerAndUuidItem(String uuidCustomer, String uuidItem);
+
+
+//    Mono<Order> findByUuidOrder(String uuidOrder);
+//
+//    Mono<Order> findByUuidCustomerAndUuidItem(String uuidCustomer, String uuidItem);
+
 }
