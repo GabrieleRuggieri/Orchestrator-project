45e2c30a89dfc5f4ac557fa7149f29c2a67bfc14
GabrieleRuggieri <gabri20022002@gmail.com>
2024-11-21 19:58:57 +0100

    New dto for payment

--- common/src/main/java/com/gabriele/common/dto/PaymentRequestDTO.java
+++ common/src/main/java/com/gabriele/common/dto/PaymentRequestDTO.java
@@ -2,11 +2,14 @@ package com.gabriele.common.dto;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 @Data
 public class PaymentRequestDTO {
-    private Integer customerId;
-    private UUID orderId;
-    private Double amount;
+
+    private String uuidCustomerAccount;
+    private String uuidOrder;
+    private BigDecimal amount;
+
 }
--- common/src/main/java/com/gabriele/common/dto/PaymentResponseDTO.java
+++ common/src/main/java/com/gabriele/common/dto/PaymentResponseDTO.java
@@ -2,14 +2,17 @@ package com.gabriele.common.dto;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 import com.gabriele.common.enums.PaymentStatus;
 
 @Data
 public class PaymentResponseDTO {
-    private Integer customerId;
-    private UUID orderId;
-    private Double amount;
+
+    private String uuidCustomerAccount;
+    private String uuidOrder;
+    private BigDecimal amount;
     private PaymentStatus status;
+
 }
