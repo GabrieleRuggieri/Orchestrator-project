2c0d2e00f2b470000fc5dc4f0fa29b3bd6050a2d
GabrieleRuggieri <gabri20022002@gmail.com>
2024-11-21 17:38:43 +0100

    Swagger

--- payment_service/src/main/java/com/gabriele/configuration/SwaggerOpenApiConfig.java
+++ payment_service/src/main/java/com/gabriele/configuration/SwaggerOpenApiConfig.java
@@ -3,32 +3,33 @@ package com.gabriele.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 
 @Configuration
-@SecurityScheme(
-		  name = "Bearer Authentication",
-		  type = SecuritySchemeType.HTTP,
-		  bearerFormat = "JWT",
-		  scheme = "bearer"
-		)
 public class SwaggerOpenApiConfig {
 
 	@Bean
 	public OpenAPI springOpenAPI() {
-		final String securitySchemeName = "bearerAuth";
 		return new OpenAPI()
-				.info(new Info().title("SpringBoot API")
-						.description("SpringBoot sample application")
+				.info(new Info()
+						.title("SpringBoot API")
+						.description("Order Management System Application")
 						.version("v0.0.1")
-						.license(new License().name("Apache 2.0").url("http://springdoc.org")))
+						.license(new License()
+								.name("Apache 2.0")
+								.url("https://opensource.org/licenses/Apache-2.0"))
+						.contact(new Contact()
+								.name("Gabriele Ruggieri")
+								.email("ruggierigabriele2002@gmail.com")))
 				.externalDocs(new ExternalDocumentation()
 						.description("SpringBoot Wiki Documentation")
 						.url("https://springboot.wiki.github.org/docs"));
 	}
 }
+
+//http://localhost:8082/swagger-ui.html
+//http://localhost:8082/v3/api-docs
