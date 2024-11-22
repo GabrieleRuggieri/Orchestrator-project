780ded7da5de8c809636fc54713fbca87a8fc199
GabrieleRuggieri <gabri20022002@gmail.com>
2024-11-22 10:23:02 +0100

    WebClient

--- orchestrator_service/src/main/java/com/gabriele/configuration/WebClientConfig.java
+++ orchestrator_service/src/main/java/com/gabriele/configuration/WebClientConfig.java
@@ -17,78 +17,78 @@ import reactor.netty.resources.ConnectionProvider;
 @Configuration
 public class WebClientConfig {
 
-	public static String INVENTORY_URL;
-	public static String PAYMENT_URL;
+    public static String INVENTORY_URL;
+    public static String PAYMENT_URL;
 
-	@Value("${service.endpoints.inventory}")
-	public void setInventoryUrl(final String propertyName) {
-		INVENTORY_URL = propertyName;
-	}
-	
-	@Value("${service.endpoints.payment}")
-	public void setPaymentUrl(final String propertyName) {
-		PAYMENT_URL = propertyName;
-	}
+    @Value("${service.endpoints.inventory}")
+    public void setInventoryUrl(final String propertyName) {
+        INVENTORY_URL = propertyName;
+    }
 
-
-	@Bean
-	public WebClient paymentClient(@Value("${service.endpoints.payment}") String endpoint){
-		return WebClient.builder()
-				.clientConnector(connector())
-				.baseUrl(endpoint)
-				.build();
-	}
-
-	@Bean
-	public WebClient inventoryClient(@Value("${service.endpoints.inventory}") String endpoint){
-		return WebClient.builder()
-				.clientConnector(connector())
-				.baseUrl(endpoint)
-				.build();
-	}
+    @Value("${service.endpoints.payment}")
+    public void setPaymentUrl(final String propertyName) {
+        PAYMENT_URL = propertyName;
+    }
 
 
-	public static WebClient paymentClient(){
-		return WebClient.builder()
-				.clientConnector(connector())
-				.baseUrl(PAYMENT_URL)
-				.build();
-	}
+    @Bean
+    public WebClient paymentClient(@Value("${service.endpoints.payment}") String endpoint) {
+        return WebClient.builder()
+                .clientConnector(connector())
+                .baseUrl(endpoint)
+                .build();
+    }
+
+    @Bean
+    public WebClient inventoryClient(@Value("${service.endpoints.inventory}") String endpoint) {
+        return WebClient.builder()
+                .clientConnector(connector())
+                .baseUrl(endpoint)
+                .build();
+    }
 
 
-	public static WebClient inventoryClient(){
-		return WebClient.builder()
-				.clientConnector(connector())
-				.baseUrl(INVENTORY_URL)
-				.build();
-	}
+    public static WebClient paymentClient() {
+        return WebClient.builder()
+                .clientConnector(connector())
+                .baseUrl(PAYMENT_URL)
+                .build();
+    }
 
 
-	private static ClientHttpConnector connector() { 	 
-		ConnectionProvider provider = ConnectionProvider
-				.builder("webclient-conn-pool")
-				.maxConnections(50)
-				.maxIdleTime(Duration.ofSeconds(60 * 4))
-				.maxLifeTime(Duration.ofSeconds(60))
-				.pendingAcquireTimeout(Duration.ofSeconds(30))
-				.pendingAcquireTimeout(Duration.ofMillis(30 * 100))                
-				.build();
+    public static WebClient inventoryClient() {
+        return WebClient.builder()
+                .clientConnector(connector())
+                .baseUrl(INVENTORY_URL)
+                .build();
+    }
 
-		HttpClient client = HttpClient.create(provider)
-				//.wiretap(true); //log				
-				.responseTimeout(Duration.ofSeconds(120)); 
 
-		ReactorClientHttpConnector conn = new ReactorClientHttpConnector(client);
+    private static ClientHttpConnector connector() {
+        ConnectionProvider provider = ConnectionProvider
+                .builder("webclient-conn-pool")
+                .maxConnections(50)
+                .maxIdleTime(Duration.ofSeconds(60 * 4))
+                .maxLifeTime(Duration.ofSeconds(60))
+                .pendingAcquireTimeout(Duration.ofSeconds(30))
+                .pendingAcquireTimeout(Duration.ofMillis(30 * 100))
+                .build();
 
-		return conn;
-	}
+        HttpClient client = HttpClient.create(provider)
+                //.wiretap(true); //log
+                .responseTimeout(Duration.ofSeconds(120));
 
-	@Bean
-	public DatabaseClient databaseClient(ConnectionFactory connectionFactory) {
-		return DatabaseClient.builder()
-				.connectionFactory(connectionFactory)
-				.namedParameters(true)
-				.build();
-	}
+        ReactorClientHttpConnector conn = new ReactorClientHttpConnector(client);
+
+        return conn;
+    }
+
+    @Bean
+    public DatabaseClient databaseClient(ConnectionFactory connectionFactory) {
+        return DatabaseClient.builder()
+                .connectionFactory(connectionFactory)
+                .namedParameters(true)
+                .build();
+    }
 
 }
