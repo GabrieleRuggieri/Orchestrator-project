641d21a5d129378729868a96428c8f6f27189110
GabrieleRuggieri <gabri20022002@gmail.com>
2024-11-22 10:20:54 +0100

    Removed static {}

--- orchestrator_service/src/main/java/com/gabriele/configuration/GlobalModelMapper.java
+++ orchestrator_service/src/main/java/com/gabriele/configuration/GlobalModelMapper.java
@@ -5,22 +5,21 @@ import org.modelmapper.convention.MatchingStrategies;
 
 public class GlobalModelMapper {
 
-	private static ModelMapper modelMapper;
+    private static ModelMapper modelMapper;
 
-	static {}
+    public static ModelMapper getModelMapper() {
 
-	public static ModelMapper getModelMapper() {
-		if (modelMapper == null) {
-			ModelMapper modelMapper = new ModelMapper();
-			modelMapper.getConfiguration()
-			.setFieldMatchingEnabled(true)
-			.setMatchingStrategy(MatchingStrategies.STRICT)
-			.setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE);
-			GlobalModelMapper.modelMapper = modelMapper;
-			return modelMapper;
-		}
-		else
-			return modelMapper;
-	}
+        if (modelMapper == null) {
+            ModelMapper modelMapper = new ModelMapper();
+            modelMapper.getConfiguration()
+                    .setFieldMatchingEnabled(true)
+                    .setMatchingStrategy(MatchingStrategies.STRICT)
+                    .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE);
+            GlobalModelMapper.modelMapper = modelMapper;
+            return modelMapper;
+        } else
+            return modelMapper;
+
+    }
 
 }
